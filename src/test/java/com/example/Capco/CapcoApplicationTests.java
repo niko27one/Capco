21dd5b4a1ba9a089e4981fefac97f742664192c7
niko27one <niko27one@live.it>
2026-09-23 17:26:48 +0200

    first commit

---
+++ src/test/java/com/example/Capco/CapcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Capco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
